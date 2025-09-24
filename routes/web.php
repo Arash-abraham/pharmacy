71444ac4bf18dc6247933a3768b3e3a48ba19a3c
Arash-abraham <arashebi777@gmail.com>
2025-09-24 21:04:02 +0330

    New UPDATE => admin panel

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\AdminDashboadrController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\AnimeController;
@@ -9,6 +10,7 @@ use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\GenreController;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PostController;
+use App\Http\Middleware\CheckAdmin;
 use App\Models\Comment;
 use Illuminate\Support\Facades\Auth;
 
@@ -38,5 +40,15 @@ Route::middleware('auth')->group(function () {
 
 Route::get('/posts/{slug}', [PostController::class, 'show'])->name('posts.show');
 Route::post('/posts/{slug}/comments', [CommentController::class, 'store'])->name('comments.store');
+
+
+// Route::prefix('admin')->middleware([CheckAdmin::class])->name('admin.')->group(function() {
+//     Route::get('/' , DashboardController::class);
+// });
+
+Route::prefix('admin')->name('admin.')->group(function() {
+    Route::get('/' , [AdminDashboadrController::class,'index']);
+});
+
 
 require __DIR__.'/auth.php';
