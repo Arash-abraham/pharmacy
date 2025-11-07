667e8f1e7a022b60cd80067709e94fdca36c5f8e
Arash <82562395+Arash-abraham@users.noreply.github.com>
2025-11-07 13:21:07 +0330

    Delete resources directory

--- resources/views/components/secondary-button.blade.php
+++
@@ -1,3 +0,0 @@
-<button {{ $attributes->merge(['class' => 'inline-flex items-center px-4 py-2 bg-purple-600 hover:bg-purple-700 text-white font-semibold rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-purple-500']) }}>
-    {{ $slot }}
-</button>
