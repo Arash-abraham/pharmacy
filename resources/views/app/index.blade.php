fc4cc5f50cf4ed224374cde290ce6f7ab535216b
Arash-abraham <arashebi777@gmail.com>
2025-12-20 21:11:51 +0330

    Setting up routes temporarily and continuing the process of migrating the front end to the Blade

--- resources/views/app/index.blade.php
+++ resources/views/app/index.blade.php
@@ -1,7 +1,7 @@
 <!DOCTYPE html>
 <html lang="fa" dir="rtl">
 <head>
-    @include('layouts.head-tag')
+    @include('app.layouts.head-tag')
 </head>
 <body class="gradient-bg">
     <!-- نوار پیشرفت -->
