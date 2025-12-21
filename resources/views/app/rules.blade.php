6daac230e6a47fe40322d8eb7a9f79aa7040ff54
Arash-abraham <arashebi777@gmail.com>
2025-12-21 22:37:40 +0330

    Migrating front-end code to Blade

---
+++ resources/views/app/rules.blade.php
@@ -0,0 +1,15 @@
+@extends('app.layouts.master')
+
+@section('title', 'قوانین')
+
+@section('css')
+    <link rel="stylesheet" href="{{ asset('css/index.css') }}">
+    <link rel="stylesheet" href="{{ asset('css/rules.css') }}">
+@endsection
+@section('content')
+
+@endsection
+
+@section('scripts')
+    <script src="{{ asset('js/rules.js') }}"></script>
+@endsection
