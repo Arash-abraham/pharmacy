0e85144fc85e89ff2895bfcbe62f5ab9ee398ec7
Arash-abraham <arashebi777@gmail.com>
2025-12-21 21:48:29 +0330

    Transfer the front-end login page code to the view section of the Laravel Breeze package and link it to the package.

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('app.home');
-}); // temp
+})->name('home'); // temp
 
 Route::get('/about',function() {
     return view('app.about');
