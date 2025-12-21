350e88de2d2a6b88254353c243e32176f0ffd55d
Arash-abraham <arashebi777@gmail.com>
2025-12-21 22:08:57 +0330

    Login and register page layout

---
+++ resources/views/auth/layouts/head-tag.blade.php
@@ -0,0 +1,8 @@
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<title>ورود / ثبت‌نام | فایل استور</title>
+<script src="https://cdn.tailwindcss.com"></script>
+<link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" rel="stylesheet">
+<link href="https://fonts.googleapis.com/css2?family=Vazirmatn:wght@100;200;300;400;500;600;700;800;900&display=swap" rel="stylesheet">
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+<link rel="stylesheet" href="{{ asset('css/login.css') }}">
